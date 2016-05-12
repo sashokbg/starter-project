65eb2a1a3713d50070af80e70235047cf594d0e8
KIRILOV-PRESTATAIRE Alexander <alexander.kirilov-prestataire@bforbank.com>
2016-05-12 16:25:05 +0200

    Added spring boot

---
+++ apps/webapps/company-web/src/main/java/bg/kirilov/company/web/conf/CompanyWebApp.java
@@ -0,0 +1,14 @@
+package bg.kirilov.company.web.conf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kirilov_a
+ */
+@SpringBootApplication
+public class CompanyWebApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyWebApp.class, args);
+    }
+}
