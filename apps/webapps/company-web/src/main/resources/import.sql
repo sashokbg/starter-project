1d0d1bf5bdf53a32ad63ebe89aaa6fba5b1b7258
KIRILOV-PRESTATAIRE Alexander <alexander.kirilov-prestataire@bforbank.com>
2016-06-16 18:30:07 +0200

    Added mysql classicmodels

--- apps/webapps/company-web/src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO address VALUES (0,'1st Bunaya str');
-INSERT INTO address VALUES (1,'22 avenue Champs Elysées');
-
-INSERT INTO city VALUES (1,'Plovdiv','Bulgaria', 0);
-INSERT INTO city VALUES (2,'Sofia','Bulgaria', 1);
-INSERT INTO city VALUES (3,'Paris','France', 0);
